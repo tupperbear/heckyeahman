127ca828ea1087a3157f3401e60addea7d49427f
Peter Tupper <p.tupper@gmail.com>
2017-06-12 22:41:56 -0300

    Adding thumbnail filtering

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 (function () {
 
+	// Init image previews
 	var elements = document.querySelectorAll( '.intense' );
 	Intense( elements );
+
+	// Init thumbnail filtering
+	var mixer = mixitup('#gallery');
+
+	// Filtering controls
+	$('.filter-control').click(function(e) {
+		e.preventDefault();
+	})
 
 })();
